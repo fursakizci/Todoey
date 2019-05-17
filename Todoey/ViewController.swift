d728d72ee45d2e1412424ca753bb7b50643c985e
furkan SAKIZCI <fursakizci@gmail.com>
2019-05-17 17:37:37 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Furkan sakızcı on 17.05.2019.
+//  Copyright © 2019 Furkan sakızcı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
