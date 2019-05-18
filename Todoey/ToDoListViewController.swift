00dd71b835dcbbe44aaace119dbf06f2ca800e37
furkan SAKIZCI <fursakizci@gmail.com>
2019-05-18 12:30:41 +0300

    Add ner item functionality complete.

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class ToDoListViewController: UITableViewController{
 
-    let itemArray = ["furkan","zeynep","ahmet"]
+    var itemArray = ["furkan","zeynep","ahmet"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
     // MARK - Tableview Datasource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -29,6 +30,8 @@ class ToDoListViewController: UITableViewController{
         return cell
     }
    
+    // MARK - Tableview Delegate Methods
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
@@ -44,6 +47,29 @@ class ToDoListViewController: UITableViewController{
         // secilen hucrenin yanip sonmesini saglar
         tableView.deselectRow(at: indexPath, animated: true)
     }
-   
+    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle:.alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what will happen once the user clicks the Add Item button on our UIAlert
+           
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+        //textfield icerisine placeholder yani silik yazi eklenir.
+        alertTextField.placeholder = "Creat New Item"
+        textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
